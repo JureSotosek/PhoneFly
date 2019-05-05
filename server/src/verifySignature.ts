85f67e5ea7b5c3a31f80b30b79996308d99f2f39
Jure Sotošek <jure.sotosek@gmail.com>
2019-05-06 00:51:20 +0200

    Minor

--- server/src/verifySignature.ts
+++ server/src/verifySignature.ts
@@ -10,28 +10,24 @@ interface RequestPayload {
 const checkSignature: (
   signedRequest: string,
 ) => RequestPayload = signedRequest => {
-  try {
-    const firstpart = signedRequest
-      .split('.')[0]
-      .replace(/-/g, '+')
-      .replace(/_/g, '/')
-    const signature = CryptoJS.enc.Base64.parse(firstpart).toString()
-    const dataHash = CryptoJS.HmacSHA256(
-      signedRequest.split('.')[1],
-      process.env.FACEBOOK_SECRET,
-    ).toString()
-    const isValid = signature === dataHash
-    const json = CryptoJS.enc.Base64.parse(
-      signedRequest.split('.')[1],
-    ).toString(CryptoJS.enc.Utf8)
+  const firstpart = signedRequest
+    .split('.')[0]
+    .replace(/-/g, '+')
+    .replace(/_/g, '/')
+  const signature = CryptoJS.enc.Base64.parse(firstpart).toString()
+  const dataHash = CryptoJS.HmacSHA256(
+    signedRequest.split('.')[1],
+    process.env.FACEBOOK_SECRET,
+  ).toString()
+  const isValid = signature === dataHash
+  const json = CryptoJS.enc.Base64.parse(signedRequest.split('.')[1]).toString(
+    CryptoJS.enc.Utf8,
+  )
 
-    if (isValid) {
-      return JSON.parse(json)
-    } else {
-      throw new Error('Signature error')
-    }
-  } catch (error) {
-    throw new Error('Signature error')
+  if (isValid) {
+    return JSON.parse(json)
+  } else {
+    throw new Error('Invalid signature')
   }
 }
 
