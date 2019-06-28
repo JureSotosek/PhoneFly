5edb0c6e0521a0fd9ba7469e472c62927654cf62
Jure Sotošek <jure.sotosek@gmail.com>
2019-06-28 23:00:32 +0200

    Remove CRA scafolding

--- app/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const App: React.FC = () => {
-  return <div>PhoneFly</div>
-}
-
-export default App
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,5 +1,8 @@
-import App from 'App'
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+function App() {
+  return <div>PhoneFly 2.0</div>
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
